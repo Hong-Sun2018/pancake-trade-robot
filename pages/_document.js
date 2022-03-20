b49bca74cfb4c8e7feec084910222d8e30687f0f
Hong-Sun2018 <sunhong_ra@live.cn>
2022-03-20 22:44:45 +0100

    test

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () => originalRenderPage({
+    ctx.renderPage = () =>git originalRenderPage({
       enhanceApp: App => props => sheets.collect(<App {...props} />)
     });
 
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Box, Button } from '@mui/material';
 import { useWallet } from 'use-wallet';
 import account from '../store/account';
 import contract from '../store/contract';
+import { useState } from 'react';
 
 const useStyles = makeStyles({
   root: {
@@ -20,21 +21,32 @@ const Home = () => {
 
   const classes = useStyles();
   const wallet = useWallet();
-  const loginAccount = account.useState('account') ;
+  const loginAccount = account.useState('account');
   const pancakeContract = contract.useState('contract');
   console.log(loginAccount);
   console.log(pancakeContract);
 
+  const [res, setRes] = useState({});
+  if (pancakeContract && pancakeContract.methods) {
+    const response = pancakeContract.methods.balanceOf(loginAccount).send();
+    setRes(response);
+  }
+
+  console.log(response);
+
+
   return (
     <Box className={classes.root}>
-      Home 
+      Home
       <Box>
         {`Account: ${loginAccount}`}
-      
+
       </Box>
       <Box>
         {`Balance: ${pancakeContract}`}
-        
+      </Box>
+      <Box>
+        {`pancake balance: ${res}`}
       </Box>
       <Box>
         <Button onClick={() => wallet.connect()} > MetaMask</Button>
