163bef3f9a1154e4f347387ab1886264ff4427cc
Hong-Sun2018 <sunhong_ra@live.cn>
2022-03-22 09:31:50 +0100

    1

--- api/pancake-api.js
+++ api/pancake-api.js
@@ -3,10 +3,14 @@ import { Contract } from 'web3-eth-contract';
 const setAllownce = (contract, account) => {
 
   const res = contract.methods.approve(account.account, 10).send({from: account.account}).then(res => {
-    console.log()
+    console.log(res)
   });
   
   return res;
 }
 
 export { setAllownce };
+
+const swap = (contract, account, toAddress) => {
+  contract.methods.swap()
+}
